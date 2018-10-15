531ba520c73233f9393a69937fd4233a618258aa
Mimi Nguyen <mytmimi@gmail.com>
2018-10-15 01:32:51 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,12 +15,13 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  
+  return num1 % num2
 end
 
 def square_root(num)
+  return Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  return num1 + (( num2 * num3 ) / num4 )
 end
